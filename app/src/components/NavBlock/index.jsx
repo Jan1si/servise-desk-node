095a76d2f938024c15e00c71477150eee30d0f75
Jan1si <nikita.evgrafov@inbox.ru>
2023-01-26 13:57:45 +0500

    add icons

--- app/src/components/NavBlock/index.jsx
+++ app/src/components/NavBlock/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from "./NavBlock.module.scss";
 import { Link } from 'react-router-dom';
+import { ReactComponent as WriteTasksIcon} from "../../assets/write-task.svg" 
+import { ReactComponent as UserIcon } from "../../assets/user.svg";
+import { ReactComponent as RolesIcon } from "../../assets/roles.svg";
+import { ReactComponent as DepartmentIcon } from "../../assets/department.svg";
+import { ReactComponent as CategoryIcon } from "../../assets/category.svg";
+
 
 export const NavBlock = () => {
   return (
@@ -8,17 +14,45 @@ export const NavBlock = () => {
       <nav className={styles.navMenu}>
       
         <ul className={styles.navList}>
-          <li className={styles.navItem}><Link to="/">Составить заявку</Link></li>
-          <li className={styles.navItem}><Link to="/tasks">Журнал заявок</Link></li>
+          <li className={styles.navItem}>
+            <Link to="/">
+              <WriteTasksIcon  className={styles.navIcon} width="25px" height="25px"/>
+              Составить заявку
+            </Link></li>
+          <li className={styles.navItem}>
+              <Link to="/tasks">
+                Журнал заявок 
+              </Link>
+            </li>
         </ul>
 
         <ul className={styles.subNavList}>
           <p className={styles.titleSubList}>Справочники</p>
 
-          <li className={styles.subNavItem}><Link to="/tables/users">Пользователи</Link></li>
-          <li className={styles.subNavItem}><Link to="/tables/departments">Отделения</Link></li>
-          <li className={styles.subNavItem}><Link to="/tables/categories">Категории</Link></li>
-          <li className={styles.subNavItem}><Link to="/tables/roles">Роли</Link></li>
+          <li className={styles.subNavItem}>
+            <Link to="/tables/users">
+              <UserIcon className={styles.navIcon} width="20px" height="20px"/>
+              Пользователи
+              </Link>
+          </li>
+          <li className={styles.subNavItem}>
+            <Link to="/tables/departments">
+              <DepartmentIcon className={styles.navIcon} width="20px" height="20px"/>
+              Отделения
+              </Link>
+          </li>
+          <li className={styles.subNavItem}>
+            <Link to="/tables/categories">
+              <CategoryIcon className={styles.navIcon} width="20px" height="20px"/>
+              Категории
+              </Link>
+          </li>
+          <li className={styles.subNavItem}>
+            <Link to="/tables/roles">
+              <RolesIcon className={styles.navIcon} width="20px" height="20px"/>
+              Роли
+              </Link>
+          </li>
 
         </ul>
 
--- app/src/components/TasksBar/index.jsx
+++ app/src/components/TasksBar/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import empty from '../../assets/empty.svg';
+import styles from './TaskBar.module.scss';
 
 export const TasksBar = () => {
   return (
-    <div className="blockTasks">
-      <div className="title">
+    <div className={styles.blockTasks}>
+      <div className={styles.title}>
         <h2>Все ваши заявки</h2>
       </div>
-      <div className="tasksList">
-        <div className="emptyBlock">
+      <div className={styles.tasksList}>
+        <div className={styles.emptyBlock}>
           <img src={empty} alt="Пусто" />
-          <p className="emptyText">Список ваших заявок пуст!</p>
+          <p className={styles.emptyText}>Список ваших заявок пуст!</p>
         </div>
       </div>
     </div>
